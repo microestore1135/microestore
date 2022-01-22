b2c93c8fe7e38c293fc3edb922d9bb28d72282d1
microestore1135 <microestore1135@protonmail.com>
2022-01-22 13:04:06 +0800

    Initial commit

---
+++ src/main/java/com/monochrome/microestore/microestore/MicroestoreApplication.java
@@ -0,0 +1,13 @@
+package com.monochrome.microestore.microestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monochrome/microestore/microestore/MicroestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monochrome.microestore.microestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
